f006fa5425076362bd3e087cbd20a715a517216e
Pruthvij008 <desaipruthvij1@gmail.com>
2024-03-09 14:26:30 +0530

    live singin removed

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,27 +1,27 @@
 /* eslint-disable */
-import axios from 'axios';
+// import axios from 'axios';
 
-import { showAlert } from './alerts';
+// import { showAlert } from './alerts';
 
-export const sign = async (name, email, password, passwordConfirm) => {
-  try {
-    const res = await axios({
-      method: 'POST',
-      url: '/api/v1/users/signup',
-      data: {
-        name: name,
-        email,
-        password,
-        passwordConfirm
-      }
-    });
-    if (res.data.status === 'success') {
-      showAlert('success', 'signup sucessfull');
-      window.setTimeout(() => {
-        location.assign('/home');
-      }, 400);
-    }
-  } catch (err) {
-    showAlert('error', err.response.data.message);
-  }
-};
+// export const sign = async (name, email, password, passwordConfirm) => {
+//   try {
+//     const res = await axios({
+//       method: 'POST',
+//       url: '/api/v1/users/signup',
+//       data: {
+//         name: name,
+//         email,
+//         password,
+//         passwordConfirm
+//       }
+//     });
+//     if (res.data.status === 'success') {
+//       showAlert('success', 'signup sucessfull');
+//       window.setTimeout(() => {
+//         location.assign('/home');
+//       }, 400);
+//     }
+//   } catch (err) {
+//     showAlert('error', err.response.data.message);
+//   }
+// };
